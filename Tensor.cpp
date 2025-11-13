7030cc832e2137296863659ae481f2fd3081ddd2
tymek <st55642@zut.edu.pl>
2025-11-13 16:22:25 +0100

    initial commit

---
+++ Tensor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by chomi on 13.11.2025.
+//
+
+#include "Tensor.h"
---
+++ Tensor.h
@@ -0,0 +1,16 @@
+//
+// Created by chomi on 13.11.2025.
+//
+
+#ifndef TENSOR_H
+#define TENSOR_H
+
+
+
+class Tensor {
+
+};
+
+
+
+#endif //TENSOR_H
